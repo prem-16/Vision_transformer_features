be6b4e207024f3a558a30fd31421a9dddf854cc5
Kathapet Nawongs <tigerbee.nawongs@gmail.com>
2023-07-05 17:10:22 +0200

    generate descriptors

--- src/dataset_collection/generate_descriptors.py
+++ src/dataset_collection/generate_descriptors.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse 
 import gzip
 import pickle
 from src.dataset_collection.generate_dataset import store_data
@@ -8,23 +8,24 @@ from src.gui.main import model_manager
 from src.models.model_gui_manager import ModelGUIManager
 
 
-def generate_descriptors(class_type=None, settings=None, dataset_path=None, descriptor_out=None):
+def generate_descriptors(model='DinoViT', stride=4, load_size=324, layer=11, facet='key', threshold=0.05, model_type='dino_vits8', log_bin=0, dataset_path='./test_data', descriptor_out='./test_data/descriptors'):
     """
     Generate the descriptors from the generated dataset.
     """
     model_manager = ModelGUIManager()
-    model_manager.update_model("DinoViT")
-    model_manager.apply_setting("stride", 4)
-    model_manager.apply_setting("load_size", 324)
-    model_manager.apply_setting("layer", 11)
-    model_manager.apply_setting("facet", "key")
-    model_manager.apply_setting("threshold", 0.05)
-    model_manager.apply_setting("model_type", "dino_vits8")
-    model_manager.apply_setting("log_bin", 0)
+    model_manager.update_model(model)
+    model_manager.apply_setting("stride", stride)
+    model_manager.apply_setting("load_size", load_size)
+    model_manager.apply_setting("layer", layer)
+    model_manager.apply_setting("facet", facet)
+    model_manager.apply_setting("threshold", threshold)
+    model_manager.apply_setting("model_type", model_type)
+    model_manager.apply_setting("log_bin", log_bin)
     descriptor_list = []
     # Load the dataset
-    data_file = gzip.open(dataset_path, 'rb')
-    data = pickle.load(data_file)
+    data_file = os.path.join(dataset_path, 'data.pkl.gzip')
+    f = gzip.open(dataset_path, 'rb')
+    data = pickle.load(f)
     number_of_images = len(data)
     # Iterate over the images
     for i in range(number_of_images):
@@ -36,22 +37,34 @@ def generate_descriptors(class_type=None, settings=None, dataset_path=None, desc
         #   Append descriptor to list
         descriptor_list.append(descriptor)
     # Save descriptors list
-    store_data(descriptor_list, "./test_data/descriptors/")
+    store_data(descriptor_list, descriptor_out)
 
 
 if __name__ == '__main__':
     # Use argparse to get the arguments
     arg = argparse.ArgumentParser()
-    # Class type
-    arg.add_argument('--class_type', type=str)
-    # Settings class
-    arg.add_argument('--settings', type=str, default='settings')
-    # Dataset path
-    arg.add_argument('--dataset_path', type=str, default='dataset')
-    # Descriptor output
-    arg.add_argument('--descriptor_out', type=str, default='descriptors')
+    # Model 
+    arg.add_argument('--model', type=str, default='DinoViT')
+    # Stride
+    arg.add_argument('--stride', type=int, default=4)
+    # Load size
+    arg.add_argument('--load_size', type=int, default=324)
+    # Layer
+    arg.add_argument('--layer', type=int, default=11)
+    # Facet
+    arg.add_argument('--facet', type=str, default='key')
+    # Threshold
+    arg.add_argument('--threshold', type=float, default=0.05)
+    # Model Type
+    arg.add_argument('--model_type', type=str, default='dino_vits8')
+    # Log Bin
+    arg.add_argument('--log_bin', type=int, default=0)
+    # Data set path
+    arg.add_argument('--dataset_path', type=str, default='./test_data')
+    # Descriptor save output path
+    arg.add_argument('--descriptor_out', type=str, default='./test_data/descriptors')
     # Parse the arguments
     args = arg.parse_args()
 
     # Generate the descriptors
-    # TODO
+    generate_descriptors(args.model, args.stride, args.load_size, args.layer, args.facet, args.threshold, args.model_type, args.log_bin, args.dataset_path, args.descriptor_out)
