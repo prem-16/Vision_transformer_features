b2bb00f87eff6831b9d52c38070f8edb0e6ea2c1
Noah L <lenagann@cs.uni-freiburg.de>
2023-07-11 22:24:14 +0200

    Made some changes to generate descriptors.

--- src/dataset_collection/generate_descriptors.py
+++ src/dataset_collection/generate_descriptors.py
@@ -15,9 +15,8 @@ from src.models.model_wrapper_list import MODEL_DICT
 
 def generate_descriptors(
         model_wrapper: ModelWrapperBase = None,
-        dataset_path='test_data',
-        descriptor_out='test_data/descriptors',
-        descriptor_name=None,
+        dataset_path=None,
+        descriptor_path=None,
         settings=None
 ):
     """
@@ -29,15 +28,12 @@ def generate_descriptors(
     print("Current path: %s"%(os.getcwd()))
     # Assert dataset path exists
     assert os.path.exists(dataset_path), "Dataset path does not exist."
-    # Assert descriptor out path exists
-    assert os.path.exists(descriptor_out), "Descriptor out path does not exist."
-    # Assert descriptor name exists
-    assert descriptor_name is not None, "Descriptor name must exist."
+    # Assert descriptor path exists
+    assert os.path.exists(descriptor_path), "Descriptor path does not exist."
 
     descriptor_list = []
     # Load the dataset
-    data_file = os.path.join(dataset_path)
-    f = gzip.open(data_file, 'rb')
+    f = gzip.open(dataset_path, 'rb')
     data = pickle.load(f)
     number_of_images = len(data)
     # Iterate over the images
@@ -50,8 +46,16 @@ def generate_descriptors(
         descriptor = model_wrapper._compute_descriptors_from_numpy(img, **settings)
         #   Append descriptor to list
         descriptor_list.append(descriptor)
-    # Save descriptors list
-    store_data(descriptor_list, descriptor_out, descriptor_name=descriptor_name)
+
+    # Create the descriptor save dictionary
+    descriptor_save_dict = {
+        "descriptors": descriptor_list,
+        "settings": settings
+    }
+
+    # Split the descriptor path into path and file name
+    descriptor_dir, descriptor_filename = os.path.split(descriptor_path)
+    store_data(descriptor_save_dict, descriptor_dir, descriptor_name=descriptor_filename)
 
 
 if __name__ == '__main__':
@@ -62,9 +66,7 @@ if __name__ == '__main__':
     # Data set path
     arg.add_argument('--dataset_path', type=str, default='./test_data')
     # Descriptor save output path
-    arg.add_argument('--descriptor_out', type=str, default='./test_data/descriptors')
-    # Descriptor file name
-    arg.add_argument('--descriptor_name', type=str)
+    arg.add_argument('--descriptor_path', type=str, default='./test_data/descriptors')
     known_args = arg.parse_known_args()[0]
 
     # Get the model wrapper
@@ -105,7 +107,6 @@ if __name__ == '__main__':
     generate_descriptors(
         model_wrapper=model_wrapper,
         dataset_path=args.pop('dataset_path', None),
-        descriptor_out=args.pop('descriptor_out', None),
-        descriptor_name=args.pop('descriptor_name', None),
+        descriptor_path=args.pop('descriptor_path', None),
         settings=args
     )
