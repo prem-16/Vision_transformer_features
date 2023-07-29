09e2c68cdf620a0b3580bc3df8907b2a3fda30e8
noahl <noah.lenagan2175@gmail.com>
2023-07-29 21:34:33 +0200

    Pushing a change to meanplot

--- src/gui/get_mean_plot.py
+++ src/gui/get_mean_plot.py
@@ -22,7 +22,8 @@ def map_to_range_180(value):
 
 def average_error(model_configs="id_1_1", movement_type="translation_X"):
     x_list = []
-    y_list = []
+    heatmap_errors_list = []
+    max_point_errors_list = []
     i = 0
     """Iterate through all files"""
     for filename in os.listdir(performance_path + "/"):
@@ -32,10 +33,11 @@ def average_error(model_configs="id_1_1", movement_type="translation_X"):
             f = gzip.open(performance_path + "/" + filename, "rb")
             data = pickle.load(f)
             print(data)
-            x, y = zip(*data)
+            x, heatmap_errors, max_point_errors = zip(*data)
 
             x_list.append(x)
-            y_list.append(y)
+            heatmap_errors_list.append(heatmap_errors)
+            max_point_errors_list.append(max_point_errors)
             # plt.plot(x, y)
             # plt.savefig(f"{i}_{model_configs}_{movement_type}")
             # plt.close()
@@ -44,15 +46,19 @@ def average_error(model_configs="id_1_1", movement_type="translation_X"):
     print("Number of episodes ", i)
     """Average all episodes y and x values"""
 
+    if len(x_list) == 0:
+        return -1, -1
+
     x_mean = np.mean(x_list, axis=0)
     if movement_type.startswith("rotation"):
         x_mean = x_mean * (180 / math.pi)
         x_mean = [map_to_range_180(x) for x in x_mean]
-    y_mean = np.mean(y_list, axis=0)
+    heatmap_error_mean = np.mean(heatmap_errors_list, axis=0)
+    max_point_error_mean = np.mean(max_point_errors_list, axis=0)
     # fig, ax = plt.subplots()
     # ax.plot(x_mean, y_mean)
     # plt.savefig(f"mean_{model_configs}_{movement_type}")
-    return x_mean, y_mean
+    return x_mean, heatmap_error_mean, max_point_error_mean
 
 
 def main(model_configs, movement_type):
@@ -80,11 +86,11 @@ if __name__ == "__main__":
     ]
 
     # Config ids to plot
-    config_ids = ['id_3_3', 'id_3_5']
+    # config_ids = ['(id_1_1)', '(id_1_2)', '(id_1_4)', '(id_1_5), (id_1_6)']
     # config_ids = ['id_1_6', 'id_3_2']
     # config_ids = ['id_1_1', 'id_1_1_2']
     # Filter configs by keys that contain a string from configs_ids
-    configs = {k: v for k, v in configs.items() if any(x in k for x in config_ids)}
+    # configs = {k: v for k, v in configs.items() if any(x in k for x in config_ids)}
 
 
     for transformation in transformations:
@@ -92,9 +98,10 @@ if __name__ == "__main__":
         plt.figure()
         for config_id, config in configs.items():
             print(config_id)
-            x_mean, y_mean = average_error(model_configs=config_id, movement_type=transformation)
+            x_mean, heatmap_error_mean, max_point_error_mean = \
+                average_error(model_configs=config_id, movement_type=transformation)
 
-            y_av = movingaverage(y_mean, 1)
+            y_av = movingaverage(np.log(heatmap_error_mean), 1)
             plt.title(f"{transformation}")
             plt.plot(x_mean,
                      y_av, label=config["exp_name"])
@@ -102,12 +109,13 @@ if __name__ == "__main__":
             # plt.ylim(0.1, 0.5)
             plt.xlabel("Change in camera position(absolute value)")
             plt.ylabel("Mean error")
-            plt.legend(loc='upper right', bbox_to_anchor=(1.6, 1.05))
+            plt.legend(loc='upper center', bbox_to_anchor=(0.5,-0.1))
         plt.savefig(f"plots/mean_ALL_MODELS_{transformation}", bbox_inches='tight')
 
-        x_mean, y_mean = average_error(model_configs=config_id, movement_type=args.movement)
+        x_mean, heatmap_error_mean, max_point_error_mean = \
+            average_error(model_configs=config_id, movement_type=args.movement)
 
-        y_av = movingaverage(y_mean, 2)
+        y_av = movingaverage(np.log(heatmap_error_mean), 2)
         plt.plot(x_mean,
                  y_av, label=id)
         plt.xlim(-0.2, 0.2)
