fb17d399339567d97dc4cb401810c993b8aad3bf
Premnath <sriprem1130@gmail.com>
2023-07-29 21:57:37 +0000

    added plots

--- src/gui/get_mean_plot.py
+++ src/gui/get_mean_plot.py
@@ -31,11 +31,11 @@ def average_error(model_configs="id_1_1", movement_type="translation_X"):
             # print(filename)
             f = gzip.open(performance_path + "/" + filename, "rb")
             data = pickle.load(f)
-            print(data)
-            x, y = zip(*data)
 
-            x_list.append(x)
-            y_list.append(y)
+            translation_list, error_list_heatmap, error_list_max_point = zip(*data)
+
+            x_list.append(translation_list)
+            y_list.append(error_list_heatmap)
             # plt.plot(x, y)
             # plt.savefig(f"{i}_{model_configs}_{movement_type}")
             # plt.close()
@@ -49,10 +49,11 @@ def average_error(model_configs="id_1_1", movement_type="translation_X"):
         x_mean = x_mean * (180 / math.pi)
         x_mean = [map_to_range_180(x) for x in x_mean]
     y_mean = np.mean(y_list, axis=0)
+    y_std = np.std(y_list, axis=0)
     # fig, ax = plt.subplots()
     # ax.plot(x_mean, y_mean)
     # plt.savefig(f"mean_{model_configs}_{movement_type}")
-    return x_mean, y_mean
+    return x_mean, y_mean, y_std
 
 
 def main(model_configs, movement_type):
@@ -80,21 +81,21 @@ if __name__ == "__main__":
     ]
 
     # Config ids to plot
-    config_ids = ['id_3_3', 'id_3_5']
+    # config_ids = ['id_3_3', 'id_3_5']
     # config_ids = ['id_1_6', 'id_3_2']
-    # config_ids = ['id_1_1', 'id_1_1_2']
+    config_ids = ['id_1_1', 'id_1_4']
+    #config_ids = ['id_1_4', 'id_1_1_2']
     # Filter configs by keys that contain a string from configs_ids
     configs = {k: v for k, v in configs.items() if any(x in k for x in config_ids)}
-
 
     for transformation in transformations:
         print(transformation)
         plt.figure()
         for config_id, config in configs.items():
             print(config_id)
-            x_mean, y_mean = average_error(model_configs=config_id, movement_type=transformation)
+            x_mean, y_mean, y_std = average_error(model_configs=config_id, movement_type=transformation)
 
-            y_av = movingaverage(y_mean, 1)
+            y_av = movingaverage(y_mean, 2)
             plt.title(f"{transformation}")
             plt.plot(x_mean,
                      y_av, label=config["exp_name"])
@@ -102,15 +103,16 @@ if __name__ == "__main__":
             # plt.ylim(0.1, 0.5)
             plt.xlabel("Change in camera position(absolute value)")
             plt.ylabel("Mean error")
+            plt.fill_between(x_mean, y_av - y_std, y_av + y_std, alpha=0.3)
             plt.legend(loc='upper right', bbox_to_anchor=(1.6, 1.05))
         plt.savefig(f"plots/mean_ALL_MODELS_{transformation}", bbox_inches='tight')
 
-        x_mean, y_mean = average_error(model_configs=config_id, movement_type=args.movement)
-
-        y_av = movingaverage(y_mean, 2)
-        plt.plot(x_mean,
-                 y_av, label=id)
-        plt.xlim(-0.2, 0.2)
-        plt.legend()
+        # x_mean, y_mean = average_error(model_configs=config_id, movement_type=args.movement)
+        #
+        # y_av = movingaverage(y_mean, 2)
+        # plt.plot(x_mean,
+        #          y_av, label=id)
+        # plt.xlim(-0.2, 0.2)
+        # plt.legend()
 
     # plt.savefig(f"plots/mean_ALL_MODELS_{args.movement}")
